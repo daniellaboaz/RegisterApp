92f7dfbab6ff8d01a128ed84e1dd04001d682f72
daniellaboaz <125586076+daniellaboaz@users.noreply.github.com>
2024-03-07 17:15:45 +0200

    Delete new-folder directory

--- new-folder/src/routes/user.js
+++
@@ -1,6 +0,0 @@
-const { userHelloWorld } = require("../controller/userController");
-
-const router = require("express").Router();
-
-router.get("/helloWorld", userHelloWorld);
-module.exports = router;
--- new-folder/src/routes/weather.js
+++
@@ -1,6 +0,0 @@
-const { userGetWeather } = require("../controller/weatherController");
-
-const router = require("express").Router();
-
-router.get("/:location", userGetWeather);
-module.exports = router;
